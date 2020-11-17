3b161362b91c250f21b0a7901aae8489b4aa4f55
Hasan Hasanov <hasan.mehmedov@gmail.com>
2020-11-17 23:53:40 +0200

    Initiala project commit

---
+++ src/BgLandmarks/BgLandmarks/Controllers/LandmarkController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BgLandmarks.Controllers
+{
+    [ApiController]
+    [Route("landmarks")]
+    public class LandmarkController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllLandmarks()
+        {
+            await Task.Delay(1);
+            return Ok();
+        }
+    }
+}
